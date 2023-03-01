16aedab0f4bca9c7188fe85c04bf05580eca4773
Connor Sandstrom <conmansp@byu.edu>
2023-02-28 22:11:54 -0700

    Add project files.

---
+++ Mission_9_conmansp/Models/ViewModels/BooksViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission_9_conmansp.Models.ViewModels
+{
+    public class BooksViewModel
+    {
+        public IQueryable<Book> Books { get; set; }
+        public Pageinfo PageInfo { get; set; }
+    }
+}
---
+++ Mission_9_conmansp/Models/ViewModels/Pageinfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission_9_conmansp.Models.ViewModels
+{
+    public class Pageinfo
+    {
+        public int TotalNumBooks { get; set; }
+
+        public int BooksPerPage { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        //Figure out how many pages we need
+        public int TotalPages => (int) Math.Ceiling((double) TotalNumBooks / BooksPerPage);
+    }
+}
